e1421a909413e33b09285ce6bae61ba1295c8d29
UndissyaPutri <undissyaputri@gmail.com>
2022-09-21 22:35:02 +0700

    update lab 3 malam

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from wishlist.views import show_wishlist, show_xml, show_json, show_json_by_id, show_xml_by_id
+from wishlist.views import register #sesuaikan dengan nama fungsi yang dibuat
+from wishlist.views import login_user #sesuaikan dengan nama fungsi yang dibuat
+from wishlist.views import logout_user #sesuaikan dengan nama fungsi yang dibuat
 
 app_name = 'wishlist'
 
@@ -9,4 +12,7 @@ urlpatterns = [
     path('json/', show_json, name='show_json'),
     path('xml/<int:id>', show_xml_by_id, name='show_xml_by_id'),
     path('json/<int:id>', show_json_by_id, name='show_json_by_id'),
+    path('register/', register, name='register'), #sesuaikan dengan nama fungsi yang dibuat
+    path('login/', login_user, name='login'), #sesuaikan dengan nama fungsi yang dibuat   
+    path('logout/', logout_user, name='logout'), #sesuaikan dengan nama fungsi yang dibuat
 ]
